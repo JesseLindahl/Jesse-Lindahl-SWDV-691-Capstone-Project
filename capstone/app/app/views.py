6891d80c3911c0c13099cdd237541bb0568137b3
Jesse Lindahl <92756804+JesseLindahl@users.noreply.github.com>
2022-07-31 21:44:38 -0500

    Update to the capstone Django framework

---
+++ capstone/app/app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    #Needed for data to be passed through 
+    context = {}
+    return render(request, 'app/index.html', context)
+
+def account(request):
+    context = {}
+    return render(request, 'app/account.html', context)
+    
+def cancel(request):
+    context = {}
+    return render(request, 'app/cancel.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'app/cart.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'app/contact.html', context)
+
+def product(request):
+    context = {}
+    return render(request, 'app/product.html', context)
+
+def store(request):
+    context = {}
+    return render(request, 'app/store.html', context)
+
+def success(request):
+    context = {}
+    return render(request, 'app/success.html', context)
+
+def workout(request):
+    context = {}
+    return render(request, 'app/workout.html', context)
