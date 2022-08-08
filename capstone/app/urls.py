c7441fb84f84afdf4f0e320be4b1c165bdba77d2
Jesse Lindahl <92756804+JesseLindahl@users.noreply.github.com>
2022-08-07 21:31:21 -0500

    SPRINT 4 Updates

--- capstone/app/urls.py
+++ capstone/app/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    
+    path('', views.main, name="main"),
+    path('index/', views.index, name="index"),
+    path('account/', views.account, name="account"),
+    path('cancel/', views.cancel, name="cancel"),
+    path('cart/', views.cart, name="cart"),
+    path('contact/', views.contact, name="contact"),
+    path('product/', views.product, name="product"),
+    path('store/', views.store, name="store"),
+    path('success/', views.success, name="success"),
+    path('workout/', views.workout, name="workout"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- capstone/app/views.py
+++ capstone/app/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    #Needed for data to be passed through 
+    context = {}
+    return render(request, 'app/main.html', context)
+
+def index(request):
+    context = {}
+    return render(request, 'app/index.html', context)
+
+def account(request):
+    context = {}
+    return render(request, 'app/account.html', context)
+    
+def cancel(request):
+    context = {}
+    return render(request, 'app/cancel.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'app/cart.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'app/contact.html', context)
+
+def product(request):
+    context = {}
+    return render(request, 'app/product.html', context)
+
+def store(request):
+    context = {}
+    return render(request, 'app/store.html', context)
+
+def success(request):
+    context = {}
+    return render(request, 'app/success.html', context)
+
+def workout(request):
+    context = {}
+    return render(request, 'app/workout.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'app/checkout.html', context)
