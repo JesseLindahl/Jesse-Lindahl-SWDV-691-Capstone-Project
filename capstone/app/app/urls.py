357d662bae650190b1a5163ee0ed1bdc7037a658
Jesse Lindahl <92756804+JesseLindahl@users.noreply.github.com>
2022-08-14 11:04:16 -0500

    Sprint 5 Updates

--- capstone/app/app/urls.py
+++ capstone/app/app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.main, name="main"),
+    path('index/', views.index, name="index"),
     path('account/', views.account, name="account"),
     path('cancel/', views.cancel, name="cancel"),
     path('cart/', views.cart, name="cart"),
@@ -11,4 +12,7 @@ urlpatterns = [
     path('store/', views.store, name="store"),
     path('success/', views.success, name="success"),
     path('workout/', views.workout, name="workout"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
 ]
